9b24869361452872fd0a4df80d6dcffb096f0c0f
Kang Chou <kangchou666@gmail.com>
2020-03-29 21:42:26 +0800

    list

---
+++ list.py
@@ -0,0 +1,11 @@
+def dedupe(items):
+    seen = set()
+    for i in items:
+        if i not in seen:
+            yield i  #输出i
+            seen.add(i)
+
+if __name__ == "__main__":
+    y = [1,4,9,5,5,8,3]
+    print (y)
+    print(list(dedupe(y)))
---
+++ list2.py
@@ -0,0 +1,18 @@
+def funha(items, key=None):
+    seen = set()
+    for i in items:
+        a = i if key is None else key(i)
+        if a not in seen:
+            yield i
+            seen.add(a)
+
+if __name__ == "__main__":
+    b = [{'x': 6, 'y': 72},
+        {'x': 16, 'y': 12},
+        {'x': 26, 'y': 52},
+        {'x': 36, 'y': 32}]
+    print (b)
+    print(list(funha(b, key=lambda b: (b['x'], b['y']))))
+   
+   [{'x': 6, 'y': 72}, {'x': 16, 'y': 12}, {'x': 26, 'y': 52}, {'x': 36, 'y': 32}]
+[{'x': 6, 'y': 72}, {'x': 16, 'y': 12}, {'x': 26, 'y': 52}, {'x': 36, 'y': 32}]
